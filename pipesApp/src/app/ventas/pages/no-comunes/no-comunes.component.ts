83cb38de8df298220b53a70af4fb70f86c18ce12
Joel Uzcategui <JoelUzcategui@protonmail.com>
2021-04-26 13:16:20 -0400

    fin seccion 10

--- pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -35,4 +36,43 @@ export class NoComunesComponent implements OnInit
   ngOnInit() {
     this.primengConfig.ripple = true;
   }
+
+  cambiarPersona(){
+    this.nombre = "asdasdasdasd";
+    this.genero = "femenino";
+  }
+
+  borrarCliente(){
+    this.clientes.pop();
+  }
+
+  //KeyValuePipe
+  personas = {
+    nombre: 'Fernando',
+    edad: 35,
+    direccion: "OT CANADA"
+  }
+
+  heroes = [
+    {
+      nombre: "Superman",
+      vuela: true
+    },
+    {
+      nombre: "Robin",
+      vuela: false
+    },
+    {
+      nombre: "Aquaman",
+      vuela: false
+    }
+  ];
+
+  miObservable = interval(1000);
+
+  valorPromesa = new Promise( (resolve, reject) => {
+    setTimeout(() =>{
+      resolve('Tenemos data de la promesa');
+    }, 3500);
+  } );
 }
