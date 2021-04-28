848cc732c28fec95536c154ccdcd90e2acd514c8
arianigarciam <arianigarciam@gmail.com>
2021-04-28 15:40:20 -0400

    clase 161

---
+++ pipesApp/src/app/ventas/pipes/mayusculas.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: 'mayusculas'
+})
+
+export class MayusculasPipe implements PipeTransform {
+    
+    transform(valor:string, enMayusculas:Boolean=true): string {
+       
+        // if (enMayusculas === true) {
+        //     return valor.toUpperCase(); 
+        // }
+        // else {
+        //     return valor.toLowerCase();
+        // }
+
+        return (enMayusculas) 
+        ? valor.toUpperCase()
+        : valor.toLowerCase();
+
+    }
+
+
+
+}
+
